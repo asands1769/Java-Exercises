1d70f992fba90ccd73f15366de5930a1745f7705
asands1769 <asands1769@outlook.com>
2023-02-01 18:19:34 -0600

    Quiz studio bug fixed

--- Quiz/src/main/java/org/example/Quiz.java
+++ Quiz/src/main/java/org/example/Quiz.java
@@ -6,27 +6,29 @@ import java.util.Scanner;
 
 public class Quiz {
 
-    private static ArrayList<Question> quizQuestions = new ArrayList<>();
     private static double scorePercentage;
+    private static ArrayList<Question> quizQuestions = new ArrayList<>();
+
     public static ArrayList<Question> getQuizQuestions() {
         return quizQuestions;
+    }
+
+
+
+    public static void addQuizQuestion(Question question){
+        quizQuestions.add(question);
     }
 
     public static double getScorePercentage() {
         return scorePercentage;
     }
 
-    public static void setScorePercentage(double score) {
-        scorePercentage = score;
+    public static void setScorePercentage(double scorePercentage) {
+        Quiz.scorePercentage = scorePercentage;
     }
-
-    public static void addQuizQuestion(Question question){
-        quizQuestions.add(question);
-    }
-
 
     public static void startQuiz(){
-        int score = 0;
+        double score = 0;
         MultipleChoice dotNotation = new MultipleChoice("To call a method on a class without creating an instance of an object using dot notation, the method must be what? \n  A: Static     B: Private \n  C: Final     D: Not possible", "A");
         Checkbox hashMapMethods = new Checkbox("Which of the following are methods of the HashMap collection?  You can enter multiple answers.  \n  A: clone()   B: clear() \n  C: entrySet() D: valueSet()", "abc");
         TrueOrFalse methodsInClasses = new TrueOrFalse("Methods cannot stand on their own—they must be part of a class.  True or False", true);
@@ -41,10 +43,12 @@ public class Quiz {
                 score++;
             }
         }
+
         double percentage = score / quizQuestions.size();
         percentage = percentage * 100;
         Quiz.setScorePercentage(percentage);
-        System.out.println("You got " + score + " questions correct out of " + quizQuestions.size() + ".  Your percentage was " + Quiz.getScorePercentage() + "%.");
+
+        System.out.println("You got " + score + " questions correct out of " + quizQuestions.size() + ".  Your percentage was " + getScorePercentage() + "%.");
 
     }
 
